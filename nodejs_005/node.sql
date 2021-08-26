00cbe7df55674ab2558e945bae058d52bdc29e8e
rudgjs8080 <kkh_8080@naver.com>
2021-08-26 11:51:20 +0900

    0826 academy nodejs 007 pug & jquery

--- nodejs_005/node.sql
+++ nodejs_005/node.sql
@@ -1,7 +1,25 @@
 use nodedb;
 
+create database nodedb;
+use nodedb;
+
 desc tbl_replies;
 
 drop table tbl_replies;
 
 select * from tbl_replies;
+
+create User node@localhost;
+show grants for 'node';
+grant all privileges on *.* to 'node';
+
+drop user node@localhost;
+
+create user node@'%';
+grant all privileges on *.* to
+'node';
+
+alter user 'node'
+identified with mysql_native_password
+by '12341234';
+flush privileges;
