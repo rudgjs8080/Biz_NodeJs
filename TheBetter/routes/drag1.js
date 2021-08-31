3e7e12f86abcd8960647a68e043244995823cb36
rudgjs8080 <kkh_8080@naver.com>
2021-09-01 02:27:33 +0900

    0831 home thebetter drag and drop

--- TheBetter/routes/drag1.js
+++ TheBetter/routes/drag1.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function(req, res, next) {
-	res.render('drag1',{title: 'Express'})
+		res.render('drag1',{title: 'Express'})
 
 })
 
